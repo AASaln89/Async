256eecd1a44c47d794657692594670918d067609
Andrey Salnikov <kvanta13@yandex.ru>
2022-01-06 23:39:16 +0300

    Добавьте файлы проекта.

---
+++ Async/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            SumAsync(a);
+            Console.ReadKey();
+        }
+        static void Sum(int a)
+        {
+            int result = 1;
+            for (int i = 1; i < a; i++)
+            {
+                result *= i;
+                Thread.Sleep(5);
+            }
+            Console.WriteLine("Резудьтат");
+            Console.WriteLine(result);
+        }
+        static async void SumAsync(int a)
+        {
+            await Task.Run(() => Sum(a));
+        }
+
+    }
+}
